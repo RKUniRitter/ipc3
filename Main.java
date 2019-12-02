d2674434d1c48e14ec23437914624e52ac928b8b
Roger Krolow <roger.krolow@gmail.com>
2019-12-02 18:26:48 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		//Cria doduasis threads concorrentes
 		Thread ta = new Thread(new Processo ("A"));
 		Thread tb = new Thread(new Processo ("B"));
 
+		//Inicia a execução das threads
 		ta.start();
 		tb.start();
 		
+		//Sincroniza as threads
 		try {
 			ta.join();
 			tb.join();
